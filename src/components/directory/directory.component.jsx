9cd415e7ae24f942192b93bcaf120a83a932c201
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2024-02-11 17:47:47 -0800

    divided into components, finished styling menu-items

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,55 @@
+import { Component } from "react";
+import "./directory.styles.scss";
+import { MenuItem } from "../menu-item/menu-item.component";
+
+
+export class Directory extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          id: 1,
+          title: 'HATS',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+        },
+        {
+          id: 2,
+          title: 'JACKETS',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+        },
+        {
+          id: 3,
+          title: 'SNEAKERS',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+        },
+        {
+          id: 4,
+          title: 'MENS',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          size: 'large'
+        },
+        {
+          id: 5,
+          title: 'WOMENS',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          size: 'large'
+        },
+      ]
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {
+          this.state.sections.map(({ id, title, imageUrl, size }) => (
+            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          ))
+        }
+      </div>
+    )
+  }
+}
