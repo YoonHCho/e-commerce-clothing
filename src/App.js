d314032baade491a567ef53201b3edf1c2427ac4
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2024-02-12 18:00:18 -0800

    added shop page with collections and items(need more work)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import { HomePage } from "./pages/homepage/homepage.component";
+import { ShopPage } from "./pages/shop/shop.component";
 // Need to import BrowserRouter in the index.js first
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
-const HatsPage = () => {
-  return <h1>This is hats!!</h1>;
-};
-const HatsDetail = props => {
-  // const nav = useLocation();
-  // const navigate = useNavigate();
-  // navigate('/topics');
-  // const param = useParams();
-  // console.log("useMatch", useMatch());
-  // console.log("props.path: ", props.path);
-  console.log("LOCATION: ", useLocation());
-  return <h1>This is detail</h1>;
-};
+// const HatsPage = () => {
+//   return <h1>This is hats!!</h1>;
+// };
+// const HatsDetail = props => {
+//   // const nav = useLocation();
+//   // const navigate = useNavigate();
+//   // navigate('/topics');
+//   // const param = useParams();
+//   // console.log("useMatch", useMatch());
+//   // console.log("props.path: ", props.path);
+//   console.log("LOCATION: ", useLocation());
+//   return <h1>This is detail</h1>;
+// };
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/hats" element={<HatsPage />} />
-        <Route path="/hats/:hatId" element={<HatsDetail />} />
+        <Route path="/shop" element={<ShopPage />} />
+
         {/* <Route path="*" element={<Navigate to="/" />}></Route> */}
       </Routes>
     </div>
