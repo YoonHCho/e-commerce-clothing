9681c996a14aa9d7e452a795049914588ceecf94
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2024-02-15 21:49:34 -0800

    completed header, partially finished with sign-in and sign-up front end

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import { FormInput } from "../form-input/form-input.component";
+import { CustomButton } from "../custom-button/custom-button.component";
+
+import "./sign-in.styles.scss";
+
+export class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ email: "", password: "" });
+  }
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit} autoComplete="off">
+          <FormInput type="email" name="email" value={this.state.email} handleChange={this.handleChange} label="E-mail" required/>
+          <FormInput type="password" name="password" value={this.state.password} handleChange={this.handleChange} label="Password" required autoComplete="false"/>
+
+          <CustomButton type="submit">Sign In</CustomButton>
+        </form>
+
+      </div>
+    )
+  }
+
+}
---
+++ src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -0,0 +1,8 @@
+import { SignIn } from "../../components/sign-in/sign-in.component";
+import "./sign-in-and-up.styles.scss";
+
+export const SignInAndUpPage = () => (
+  <div className="sign-in-and-up">
+    <SignIn />
+  </div>
+)
