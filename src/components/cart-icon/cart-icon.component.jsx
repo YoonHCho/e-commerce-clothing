a0b6e37c5e8b7470c8cdc484acc7522005d50f2c
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2024-02-20 18:23:06 -0800

    added reduce function to show the total quantities of cart items

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,11 +1,16 @@
 import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg.svg";
 // useSelector and useDispatch hooks replaces connect (of version 5)
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-icon.styles.scss";
 
 export const CartIcon = () => {
   const dispatch = useDispatch();
+  const { cartItems } = useSelector(state => state.cart);
+
+  const itemCount = cartItems.reduce((acc, curr) => {
+    return acc + curr.quantity;
+  }, 0);
 
   const handleToggle = () => {
     dispatch(toggleCartHidden())
@@ -14,7 +19,7 @@ export const CartIcon = () => {
   return (
     <div className="cart-icon" onClick={handleToggle}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   )
 }
