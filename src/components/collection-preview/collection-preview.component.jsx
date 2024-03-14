c3714a9181535ccc24ab4355aafbb636cf76776e
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2024-03-13 21:02:09 -0700

    added responsive styling

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,24 @@
 import { CollectionItem } from "../collection-item/collection-item.component";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import "./collection-preview.styles.scss";
 
-export const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
-      {
-        items
-          .filter((item, idx) => idx < 4)
-          .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))
-      }
+export const CollectionPreview = ({ title, items }) => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  return (
+    <div className="collection-preview">
+      <h1 className="title" onClick={() => navigate(`${pathname}/${title.toLowerCase()}`)}>{title.toUpperCase()}</h1>
+      <div className="preview">
+        {
+          items
+            .filter((item, idx) => idx < 4)
+            .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))
+        }
+      </div>
     </div>
-  </div>
-)
+  )
+}
